6d4fb288f4a0d2a5c7c808691af653b07ed84a90
Your Name <you@example.com>
2022-10-17 22:02:18 -0400

    AreaChart

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,26 +1,67 @@
- /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ;
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-/* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d => {
-  return {
-    // year: new Date(+d.Year, 0, 1),
-    // country: d.Entity,
-    // population: +d.Population
-  }
-}).then(data => {
-  console.log('data :>> ', data);
+// parse the date / time
+var parseTime = d3.timeParse("%Y");
 
-  // SCALES
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
 
-  // CREATE SVG ELEMENT
+// define the area
+var area = d3.area()
+    .x(function(d) { return x(d.Year); })
+    .y0(height)
+    .y1(function(d) { return y(d.Unemployment); });
 
-  // BUILD AND CALL AXES
+// define the line
+var valueline = d3.line()
+    .x(function(d) { return x(d.Year); })
+    .y(function(d) { return y(d.Unemployment); });
 
-  // LINE GENERATOR FUNCTION
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("#container").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-  // DRAW LINE
+// get the data
+d3.csv('../data/Unemployment.csv').then(function(data) {
+
+  // format the data
+  data.forEach(function(d) {
+      d.Year = parseTime(d.Year);
+      d.Unemployment = +d.Unemployment;
+  });
+
+  // scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.Year; }));
+  y.domain([0, d3.max(data, function(d) { return d.Unemployment; })]);
+
+  // add the area
+    svg.append("path")
+       .data([data])
+       .attr("class", "area")
+       .attr("d", area);
+
+  // add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", valueline);
+
+  // add the X Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // add the Y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
 });
