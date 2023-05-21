9fae1f32a8e8e74020268f26d5b4909c190fb137
Your Name <you@example.com>
2023-05-21 13:42:33 -0400

    commit

--- Studio_Project/Final/migration.js
+++ Studio_Project/Final/migration.js
@@ -1,5 +1,5 @@
  // SVG element
- const width = 1200;
+ /*const width = 1200;
  const height = 1200;
  const margin = 400;
 
@@ -152,4 +152,164 @@
              .style("fill", d.value ? colorM(d.value) : 'white')
              .attr("width", 1.5 * d.w)
              .attr("height", 1.5 * d.w)
- }
+ } */
+// SVG element
+const width = 1200;
+const height = 1200;
+const margin = 400;
+
+// Color scale for nodes
+const colorM = d3.scaleSqrt().range(['lightyellow', 'red']);
+
+const nodes = [];
+
+const formatM = d3.format(',.2f');
+
+// Loading data
+d3.csv("EU_migration_fix_new1.csv", function (row) {
+        nodes.push({ node: row.Destination, total: +row.Total, code: row.Code, pop: +row.Population });
+
+        const line = [];
+        for (key in row) {
+                if (key != 'Population' && key != 'Destination' && key != 'Total' && key != 'Code') {
+                        if (isNaN(row[key]) || +row[key] <= 0) continue;
+                        const obj = {
+                                value: +row[key],
+                                target: row.Destination,
+                                source: key
+                        }
+                        line.push(obj);
+                }
+        }
+        return line;
+
+}).then(function (data) {
+        // Edges {source, target, value},
+        const edges = d3.merge(data);
+
+        const filteredNodes = nodes.filter(d => d.total >= 0 || d.pop >= 0)
+                .sort((a, b) => d3.descending(a.total, b.total)); // states with the most migration "action" should be in the top left corner
+        // Edges {source, target, value}, referencing to an element in the nodes array
+        const filteredEdges = edges.filter(edge => filteredNodes.find(n => n.node == edge.target))
+                .filter(edge => filteredNodes.find(n => n.node == edge.source));
+
+        filteredEdges.forEach(function (edge) {
+                edge.source = filteredNodes.map(n => n.node).indexOf(edge.source);
+                edge.target = filteredNodes.map(n => n.node).indexOf(edge.target);
+        });
+        // Fitting the matrix to SVG
+        const matrixLayout = dvj.adjacencyMatrix().size([width - margin, height - margin]);
+
+        // Bringing the matrix and providing it with data
+        const matrix = dvj.linksToMatrix(filteredNodes, filteredEdges);
+
+        const result = matrixLayout(filteredNodes, matrix);
+
+        // Setting the color scale domain
+        colorM.domain([0, d3.max(edges, d => d.value)]);
+
+        drawMigration(filteredNodes, result);
+});
+
+// Putting it all together
+function drawMigration(nodes, data) {
+
+        const svg = d3.select("#migration").append("svg").attr("width", width).attr("height", height);
+        const chart = svg.append("g").attr("transform", `translate(${[margin / 2, margin / 2]})`);
+
+        // Adding interactivity and axis labels
+        const cell = chart.selectAll('g.cell')
+                .data(data)
+                .enter()
+                .append("g").attr("class", 'cell')
+                .attr("transform", d => `translate(${[d.x, d.y]})`)
+                .on("mouseover", highlight)
+                .on("mouseout", unHighlight);
+
+        cell.append("rect")
+                .attr("height", d => d.h)
+                .attr("width", d => d.w)
+                .attr("rx", d => d.w / 4).attr("ry", d => d.h / 4)
+                .style("stroke", 'black')
+                .style("fill", d => d.value ? colorM(d.value) : 'white')
+
+        chart.selectAll('text.source')
+                .data(data.filter(d => d.y == 0))
+                .enter()
+                .append("text").attr("class", 'source')
+                .attr("y", d => d.x + d.w / 2)
+                .style("text-anchor", 'start')
+                .attr("x", 5)
+                .attr("transform", `rotate(-90) `)
+                .text((d, i) => nodes[i].node)
+                .on("mouseover", highlight)
+                .on("mouseout", unHighlight);
+        chart.append("text")
+                .style("text-anchor", 'middle')
+                .attr("transform", `rotate(-90,${[0, height / 2 - margin / 2]}) translate(${[0, height / 2 - margin / 2 - 75]})`)
+                .style("font-size", 24)
+                .text('FROM');
+
+        chart.selectAll('text.target')
+                .data(data.filter(d => d.x == 0))
+                .enter()
+                .append("text").attr("class", 'target')
+                .attr("y", d => d.y + d.h / 2)
+                .style("text-anchor", 'end')
+                .attr("x", -10)
+                .text((d, i) => nodes[i].node)
+                .on("mouseover", highlight)
+                .on("mouseout", unHighlight);
+        chart.append("text")
+                .style("text-anchor", 'middle')
+                .attr("transform", `translate(${[width / 2 - margin / 2, -75]})`)
+                .style("font-size", 24)
+                .text('TO');
+        // Adding tooltip, text showing inside the cell
+        const tooltip = chart
+                .append("g")
+                .attr("class", 'tooltip-mig tooltip-class')
+                .style("opacity", 0)
+                .style("pointer-events", 'none');
+
+        tooltip
+                .append("rect")
+                .style("fill", 'white')
+                .style("stroke", 'black');
+
+        tooltip
+                .append("text")
+                .style("font-size", 8)
+                .style("alignment-baseline", 'middle')
+                .style("text-anchor", 'middle');
+
+}
+
+// Highlight and unhighlight functions - to receive current datum and add or remove CSS classes from rect and text elements
+function unHighlight(d, i, nodes) {
+        d3.selectAll(".cell, text.source, text.target").classed('faded highlight', false);
+        d3.select('.tooltip-mig').style("opacity", 0);
+}
+
+function highlight(d) {
+        d3.selectAll(".cell").classed('faded', k => !(k.x == d.x || k.y == d.y))
+        d3.selectAll(".cell").classed('highlight', k => k.x == d.x || k.y == d.y)
+        d3.selectAll("text.source").classed('highlight', k => k.x == d.x)
+        d3.selectAll("text.target").classed('highlight', k => k.y == d.y)
+
+        d3.select('.tooltip-mig').select("text")
+                .attr("x", d.w * .75)
+                .attr("y", d.h * .75)
+                .text(d.value ? formatM(d.value / 1000) : 0);
+
+        d3.select('.tooltip-mig')
+                .attr("transform", `translate(${[d.x - d.w / 4, d.y - d.h / 4]})`)
+                .style("opacity", 1)
+                .select("rect")
+                .style("stroke-width", 3)
+                .attr("rx", d.w / 4)
+                .attr("ry", d.h / 4)
+                .style("fill", d.value ? colorM(d.value) : 'white')
+                .attr("width", 1.5 * d.w)
+                .attr("height", 1.5 * d.h);
+}
